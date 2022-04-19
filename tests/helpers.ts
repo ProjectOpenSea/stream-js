427440e9e0ced9615e684506a1dc51f9ab6c9a6c
Michael Cohen <micohen13@gmail.com>
2022-04-19 18:14:30 -0400

    Add item_metadata to Types and update README (#43)
    * Add item_metadata to Types and update README
    
    * Rename to opensea-stream-js-sdk and fix tests
    
    * Grammar fix for README
    
    * Version upgrade + rename client to OpenSeaStreamClient (#44)

--- tests/helpers.ts
+++ tests/helpers.ts
@@ -1,13 +1,13 @@
 import type { Channel, Socket } from 'phoenix';
-import { BasePushedUpdateMessage, EventType, OpenSeaPushClient } from '../src';
+import { BaseStreamMessage, EventType, OpenSeaStreamClient } from '../src';
 
-export const getSocket = (client: OpenSeaPushClient): Socket => {
+export const getSocket = (client: OpenSeaStreamClient): Socket => {
   // @ts-expect-error private access
   return client.socket;
 };
 
 export const getChannels = (
-  client: OpenSeaPushClient
+  client: OpenSeaStreamClient
 ): Map<string, Channel> => {
   // @ts-expect-error private access
   return client.channels;
@@ -18,7 +18,7 @@ type ChannelParams<Payload = unknown> = {
   ref?: string;
   topic: string;
   event: EventType;
-  payload: BasePushedUpdateMessage<Payload>;
+  payload: BaseStreamMessage<Payload>;
 };
 
 export const encode = ({
@@ -34,7 +34,7 @@ export const encode = ({
 export const mockEvent = <Payload = unknown>(
   eventType: EventType,
   payload: Payload
-): BasePushedUpdateMessage<Payload> => {
+): BaseStreamMessage<Payload> => {
   return {
     chain: {
       name: 'Ethereum'
@@ -46,6 +46,12 @@ export const mockEvent = <Payload = unknown>(
     item: {
       contract_address: '0x',
       token_id: '11'
+    },
+    item_metadata: {
+      name: 'Bored Ape #11',
+      image_url: 'https://example.com/image.png',
+      animation_url: 'https://example.com/image.png',
+      metadata_url: 'https://example.com/image.png'
     },
     payload,
     timestamp: Date.now().toString()
